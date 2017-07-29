5c4762fac1eadca04e84e1cb6901e5a0427d0b11
Michael Alley <malley@loftware.com>
2017-07-28 20:24:10 -0400

    Added 3rd party clock to my page. Working on display table and form

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAv_ar4YMftCRdvBYMB0QfZRFSVxQFtt5o",
+    authDomain: "train-schedule-80ec2.firebaseapp.com",
+    databaseURL: "https://train-schedule-80ec2.firebaseio.com",
+    projectId: "train-schedule-80ec2",
+    storageBucket: "",
+    messagingSenderId: "347865332434"
+  };
+  firebase.initializeApp(config)
+  var database=firebase.database();
+
+  
+
+
+
+});
